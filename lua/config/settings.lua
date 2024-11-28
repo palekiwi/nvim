e4fe0502d72266454b33c49efa188ad89dc99456
Pawel Lisewski <dev@palekiwi.com>
2024-11-28 21:32:07 +0800

    Highlights

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -88,3 +88,7 @@ vim.api.nvim_create_autocmd("VimEnter", {
     require("gitsigns").change_base(base_branch, true)
   end,
 })
+
+local color = vim.api.nvim_get_hl(0, { name = 'Constant' })
+vim.api.nvim_set_hl(0, "@stimulus-controller.html", { fg = color.fg, bold = true })
+vim.api.nvim_set_hl(0, "@stimulus-attribute.html", { fg = color.fg, bold = false })

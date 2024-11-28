a7ccc42506a753250b6ff4793c3061f67c949a1f
Pawel Lisewski <dev@palekiwi.com>
2024-11-28 21:31:44 +0800

    Fix highlights

--- after/queries/html/highlights.scm
+++ after/queries/html/highlights.scm
@@ -1,15 +1,21 @@
 ; extends
 
-((attribute_name) @constant
-    (#match? @constant "data-action|controller"))
+((attribute_name) @stimulus-attribute
+    (#match? @stimulus-attribute "data-action"))
 
-((attribute_name) @markup.strong
-    (#match? @markup.strong "data-controller"))
+((attribute_name) @stimulus-controller
+    (#match? @stimulus-controller "data-controller"))
 
-((attribute_name) @constant
-    (#match? @constant "data-.*-outlet|value|target"))
+((attribute_name) @stimulus-attribute
+    (#match? @stimulus-attribute "data-.*-outlet"))
 
-((attribute (attribute_name) @constant
-    ((#match? @constant "data-controller")
-    (quoted_attribute_value
-    (attribute_value) @markdown.strong))))
+((attribute_name) @stimulus-attribute
+    (#match? @stimulus-attribute "data-.*-value"))
+
+((attribute_name) @stimulus-attribute
+    (#match? @stimulus-attribute "data-.*-target"))
+
+; ((attribute (attribute_name) @constant
+;     ((#match? @constant "data-controller")
+;     (quoted_attribute_value
+;     (attribute_value) @markdown.strong))))

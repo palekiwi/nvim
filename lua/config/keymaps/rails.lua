996bca8ec208e543711c668a3f5d5814975db205
Pawel Lisewski <dev@palekiwi.com>
2024-11-30 15:50:22 +0800

    Move search group

--- lua/config/keymaps/rails.lua
+++ lua/config/keymaps/rails.lua
@@ -1,7 +1,20 @@
 local rails_utils = require('rails-utils')
 
 return {
-  { "<A-t>", rails_utils.find_template_render, desc = "Find template in views" },
-  { "<A-v>", rails_utils.find_template,        desc = "Views" },
-  { "<C-s>", rails_utils.alternate,            desc = "Find Spec" },
+  { "<A-t>",      rails_utils.find_template_render,                                       desc = "Find template in views" },
+  { "<A-v>",      rails_utils.find_template,                                              desc = "Views" },
+  { "<C-s>",      rails_utils.alternate,                                                  desc = "Find Spec" },
+  -- start group: search
+  { "<leader>s",  group = "Search" },
+  { "<leader>sC", "<cmd>Telescope find_files search_dirs=app/contracts<cr>",              desc = "Contracts" },
+  { "<leader>sS", "<cmd>Telescope find_files search_dirs=app/services<cr>",               desc = "Services" },
+  { "<leader>sc", "<cmd>Telescope find_files search_dirs=app/controllers<cr>",            desc = "Controllers" },
+  { "<leader>se", "<cmd>Telescope find_files search_dirs=webpack/src/components/elm<cr>", desc = "Elm" },
+  { "<leader>sp", "<cmd>Telescope find_files search_dirs=app/presenters<cr>",             desc = "Presenters" },
+  { "<leader>sr", "<cmd>Telescope find_files search_dirs=app/services<cr>",               desc = "Services" },
+  { "<leader>ss", "<cmd>Telescope find_files search_dirs=spec/<cr>",                      desc = "Specs" },
+  { "<leader>st", "<cmd>Telescope find_files search_dirs=webpack/src/controllers<cr>",    desc = "Stimulus" },
+  { "<leader>sv", "<cmd>Telescope find_files search_dirs=app/views<cr>",                  desc = "Views" },
+  { "<leader>sy", "<cmd>Telescope find_files search_dirs=webpack/src/styles<cr>",         desc = "Styles" },
+  -- end group: search
 }

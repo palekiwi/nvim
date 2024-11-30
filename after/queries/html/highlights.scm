e508f2f98a9318d50b68c0b8e3c58dee7783deb5
Pawel Lisewski <dev@palekiwi.com>
2024-11-30 14:46:19 +0800

    highlights.scm

--- after/queries/html/highlights.scm
+++ after/queries/html/highlights.scm
@@ -15,6 +15,9 @@
 ((attribute_name) @stimulus-attribute
     (#match? @stimulus-attribute "data-.*-target"))
 
+((attribute_name) @stimulus-attribute
+    (#match? @stimulus-attribute "data-.*-class"))
+
 ; ((attribute (attribute_name) @constant
 ;     ((#match? @constant "data-controller")
 ;     (quoted_attribute_value

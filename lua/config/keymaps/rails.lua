7697efab1286cd624b586b4c03b0d700cfabe5f3
Pawel Lisewski <dev@palekiwi.com>
2024-11-30 15:58:55 +0800

    Refactor find files

--- lua/config/keymaps/rails.lua
+++ lua/config/keymaps/rails.lua
@@ -1,20 +1,25 @@
 local rails_utils = require('rails-utils')
+local builtin = require('telescope.builtin')
+
+local function find_files(dirs)
+  builtin.find_files({ search_dirs = dirs })
+end
 
 return {
-  { "<A-t>",      rails_utils.find_template_render,                                       desc = "Find template in views" },
-  { "<A-v>",      rails_utils.find_template,                                              desc = "Views" },
-  { "<C-s>",      rails_utils.alternate,                                                  desc = "Find Spec" },
+  { "<A-t>",      rails_utils.find_template_render,                            desc = "Find template in views" },
+  { "<A-v>",      rails_utils.find_template,                                   desc = "Views" },
+  { "<C-s>",      rails_utils.alternate,                                       desc = "Find Spec" },
   -- start group: search
   { "<leader>s",  group = "Search" },
-  { "<leader>sC", "<cmd>Telescope find_files search_dirs=app/contracts<cr>",              desc = "Contracts" },
-  { "<leader>sS", "<cmd>Telescope find_files search_dirs=app/services<cr>",               desc = "Services" },
-  { "<leader>sc", "<cmd>Telescope find_files search_dirs=app/controllers<cr>",            desc = "Controllers" },
-  { "<leader>se", "<cmd>Telescope find_files search_dirs=webpack/src/components/elm<cr>", desc = "Elm" },
-  { "<leader>sp", "<cmd>Telescope find_files search_dirs=app/presenters<cr>",             desc = "Presenters" },
-  { "<leader>sr", "<cmd>Telescope find_files search_dirs=app/services<cr>",               desc = "Services" },
-  { "<leader>ss", "<cmd>Telescope find_files search_dirs=spec/<cr>",                      desc = "Specs" },
-  { "<leader>st", "<cmd>Telescope find_files search_dirs=webpack/src/controllers<cr>",    desc = "Stimulus" },
-  { "<leader>sv", "<cmd>Telescope find_files search_dirs=app/views<cr>",                  desc = "Views" },
-  { "<leader>sy", "<cmd>Telescope find_files search_dirs=webpack/src/styles<cr>",         desc = "Styles" },
+  { "<leader>sC", function() find_files({ "app/contracts" }) end,              desc = "Contracts" },
+  { "<leader>sS", function() find_files({ "app/services" }) end,               desc = "Services" },
+  { "<leader>sc", function() find_files({ "app/controllers" }) end,            desc = "Controllers" },
+  { "<leader>se", function() find_files({ "webpack/src/components/elm" }) end, desc = "Elm" },
+  { "<leader>sp", function() find_files({ "app/presenters" }) end,             desc = "Presenters" },
+  { "<leader>sr", function() find_files({ "app/services" }) end,               desc = "Services" },
+  { "<leader>ss", function() find_files({ "spec/" }) end,                      desc = "Specs" },
+  { "<leader>st", function() find_files({ "webpack/src/controllers" }) end,    desc = "Stimulus" },
+  { "<leader>sv", function() find_files({ "app/views" }) end,                  desc = "Views" },
+  { "<leader>sy", function() find_files({ "webpack/src/styles" }) end,         desc = "Styles" },
   -- end group: search
 }

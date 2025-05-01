28935edf2fd2484e2e791a37a6b63e26fe776395
Pawel Lisewski <dev@palekiwi.com>
2025-05-01 18:12:40 +0800

    ruby.snippets: use `tap` in log

--- snippets/ruby.lua
+++ snippets/ruby.lua
@@ -9,12 +9,11 @@ local r = extras.rep
 
 return {
     s("log", fmt(
-        'puts \"#{{\'{}\'.center({}, \'{}\')}}\\n"; Pry::ColorPrinter.pp({}{}); puts \"#{{\'{}\' * {}}}\\n\\n\"; # rubocop:disable Style/Semicolon',
+        '.tap {{ |val| puts \"#{{\'{}\'.center({}, \'{}\')}}\\n"; Pry::ColorPrinter.pp(val{}); puts \"#{{\'{}\' * {}}}\\n\\n\"; }} # rubocop:disable Style/Semicolon',
         {
-            r(1),
+            i(1, "val"),
             i(3,"50"),
             i(4,"-"),
-            i(1, "result"),
             c(2, {t(), t(".to_h"), t(".map(&:to_h)")}),
             r(4),
             r(3)

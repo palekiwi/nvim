d13fb4aea7f1a5177bf9151d2f477a2d2fc40872
Pawel Lisewski <dev@palekiwi.com>
2025-05-23 12:28:11 +0800

    mason-lspconfig: Disable LSPs

--- lua/config/plugins/mason-lspconfig.lua
+++ lua/config/plugins/mason-lspconfig.lua
@@ -4,14 +4,14 @@ return {
     config = function()
       require("mason-lspconfig").setup {
         ensure_installed = {
-          "ansiblels",
-          "astro",
+          -- "ansiblels",
+          -- "astro",
           "clangd",
           "elmls",
           "eslint",
-          "gopls",
-          "pyright",
-          "volar",
+          -- "gopls",
+          -- "pyright",
+          --"volar",
           "yamlls",
         },
         automatic_installation = false,

d7820ead24255362ada04543dfa472ab4cee2b0e
Pawel Lisewski <dev@palekiwi.com>
2025-08-26 19:46:15 +0800

    Add recommended grapple setup

--- lua/config/plugins/grapple.lua
+++ lua/config/plugins/grapple.lua
@@ -1,8 +1,15 @@
 return {
   {
     "cbochs/grapple.nvim",
-    dependencies = {
-        { "nvim-tree/nvim-web-devicons", lazy = true }
+    opts = {
+      scope = "git",
     },
-  },
-}
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = "Grapple",
+    keys = {
+      { "tt",         "<cmd>Grapple toggle<cr>",          desc = "Grapple toggle tag" },
+      { "<C-t>",      "<cmd>Grapple toggle_tags<cr>",     desc = "Grapple open tags window" },
+      { "<leader>fn", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple cycle next tag" },
+      { "<leader>fp", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple cycle previous tag" },
+    },
+  }, }

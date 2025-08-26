83301e4c4d9e4d5f275c15682a53831bb1c53b41
Pawel Lisewski <dev@palekiwi.com>
2025-08-26 15:57:58 +0800

    Use space for opencode

--- lua/config/plugins/opencode.lua
+++ lua/config/plugins/opencode.lua
@@ -14,13 +14,13 @@ return {
       -- Recommended keymaps
       { '<S-C-d>',    function() require('opencode').command('messages_half_page_down') end, desc = 'Scroll messages down', },
       { '<S-C-u>',    function() require('opencode').command('messages_half_page_up') end, desc = 'Scroll messages up', },
-      { '<space>eA', function() require('opencode').ask() end, desc = 'Ask opencode', },
-      { '<space>ea', function() require('opencode').ask('@cursor: ') end, desc = 'Ask opencode about this', mode = 'n', },
-      { '<space>ea', function() require('opencode').ask('@selection: ') end, desc = 'Ask opencode about selection', mode = 'v', },
-      { '<space>ee', function() require('opencode').prompt("Explain @cursor and its context") end, desc = "Explain code near cursor", },
-      { '<space>en', function() require('opencode').command('session_new') end, desc = 'New session', },
-      { '<space>ep', function() require('opencode').select_prompt() end, desc = 'Select prompt', mode = { 'n', 'v', }, },
-      { '<space>ey', function() require('opencode').command('messages_copy') end, desc = 'Copy last message', },
+      { '<space>A', function() require('opencode').ask() end, desc = 'Ask opencode', },
+      { '<space>t', function() require('opencode').ask('@cursor: ') end, desc = 'Ask opencode about this', mode = 'n', },
+      { '<space>s', function() require('opencode').ask('@selection: ') end, desc = 'Ask opencode about selection', mode = 'v', },
+      { '<space>e', function() require('opencode').prompt("Explain @cursor and its context") end, desc = "Explain code near cursor", },
+      { '<space>n', function() require('opencode').command('session_new') end, desc = 'New session', },
+      { '<space>p', function() require('opencode').select_prompt() end, desc = 'Select prompt', mode = { 'n', 'v', }, },
+      { '<space>y', function() require('opencode').command('messages_copy') end, desc = 'Copy last message', },
     },
   }
 }

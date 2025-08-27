79089caf1379d684090158348e876db9a2fca871
Pawel Lisewski <dev@palekiwi.com>
2025-08-27 12:43:34 +0800

    Grapple use_cursor

--- lua/config/plugins/grapple.lua
+++ lua/config/plugins/grapple.lua
@@ -11,7 +11,7 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     cmd = "Grapple",
     keys = {
-      { "tt",         "<cmd>Grapple toggle<cr>",                                  desc = "Grapple toggle tag" },
+      { "tt",         "<cmd>Grapple toggle use_cursor=true<cr>",                  desc = "Grapple toggle tag" },
       { "<C-t>",      "<cmd>Telescope grapple tags layout_strategy=vertical<cr>", desc = "[Telescope] Grapple open tags window" },
       { "<leader>fn", "<cmd>Grapple cycle_tags next<cr>",                         desc = "Grapple cycle next tag" },
       { "<leader>fp", "<cmd>Grapple cycle_tags prev<cr>",                         desc = "Grapple cycle previous tag" },

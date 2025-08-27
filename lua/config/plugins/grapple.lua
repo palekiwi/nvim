a9a1abe0827ae3614da587b5669115607a8a051d
Pawel Lisewski <dev@palekiwi.com>
2025-08-27 12:04:33 +0800

    Grapple telescope

--- lua/config/plugins/grapple.lua
+++ lua/config/plugins/grapple.lua
@@ -5,12 +5,15 @@ return {
     opts = {
       scope = "git_branch",
     },
+    config = function()
+      require("telescope").load_extension("grapple")
+    end,
     event = { "BufReadPost", "BufNewFile" },
     cmd = "Grapple",
     keys = {
-      { "tt",         "<cmd>Grapple toggle<cr>",          desc = "Grapple toggle tag" },
-      { "<C-t>",      "<cmd>Grapple toggle_tags<cr>",     desc = "Grapple open tags window" },
-      { "<leader>fn", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple cycle next tag" },
-      { "<leader>fp", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple cycle previous tag" },
+      { "tt",         "<cmd>Grapple toggle<cr>",                                  desc = "Grapple toggle tag" },
+      { "<C-t>",      "<cmd>Telescope grapple tags layout_strategy=vertical<cr>", desc = "[Telescope] Grapple open tags window" },
+      { "<leader>fn", "<cmd>Grapple cycle_tags next<cr>",                         desc = "Grapple cycle next tag" },
+      { "<leader>fp", "<cmd>Grapple cycle_tags prev<cr>",                         desc = "Grapple cycle previous tag" },
     },
   }, }

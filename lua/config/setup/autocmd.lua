63a9d71b257f22f1736c71d5f725c33086e4b0b2
Pawel Lisewski <dev@palekiwi.com>
2025-08-31 01:06:51 +0800

    Add set_master_branch_name

--- lua/config/setup/autocmd.lua
+++ lua/config/setup/autocmd.lua
@@ -1,11 +1,27 @@
 local create_autocmd = vim.api.nvim_create_autocmd
 
+--- Sets master branch name on startup 
+local function set_master_branch_name()
+  create_autocmd("VimEnter", {
+    callback = function()
+      local handle = io.popen("get_master_branch_name")
+      local master_branch = "master" -- fallback
+      if handle then
+        master_branch = handle:read("*a"):gsub("%s+", "")  --[[@as string]]
+        handle:close()
+      end
+
+      vim.g.git_master = master_branch
+    end,
+  })
+end
+
 --- Sets git base name on startup 
 local function set_git_base()
   create_autocmd("VimEnter", {
     callback = function()
       local handle = io.popen("get_pr_base")
-      local base_branch = "master" -- fallback
+      local base_branch = vim.g.git_master or "master" -- fallback
       if handle then
         base_branch = handle:read("*a"):gsub("%s+", "")  --[[@as string]]
         handle:close()
@@ -34,6 +50,7 @@ end
 
 
 return function()
+  set_master_branch_name()
   set_git_base()
   set_pr_number("GH_PR_NUMBER")
 
